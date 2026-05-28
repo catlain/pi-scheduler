4ec00a55ebcf9483589820d63319f335cde57feb
Lain <lain@pi.local>
2026-05-28 18:57:42 +0800

    chore: add @vitest/coverage-v8 + coverage config in createConfig

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-  test: {
-    include: ["tests/**/*.test.ts"],
-  },
+	test: {
+		include: ["tests/**/*.test.ts"],
+		coverage: {
+			provider: "v8" as const,
+			reporter: ["text", "html"],
+		},
+	},
 });
